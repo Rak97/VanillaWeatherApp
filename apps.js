da0094d974e20dbac1bad9eea07f273c1e61d35b
Rakave Kandiah <88566790+Rak97@users.noreply.github.com>
2021-11-30 19:21:18 -0500

    icon

--- apps.js
+++ apps.js
@@ -1,15 +1,22 @@
-   function displayTemperature(response) { 
+        function displayTemperature(response) { 
+            console.log(response.data);
                 let descripitionElement = document.querySelector("#descripition");
                 let cityElement = document.querySelector("#city");
                 let temperatureElement = document.querySelector("#temperature");
                 let humidityElement = document.querySelector("#humidity");
                 let windElement = document.querySelector("#wind");
+                let iconElement = document.querySelector("#icon");
                 document.getElementById("year").innerHTML = new Date().getFullYear();
                 temperatureElement.innerHTML = Math.round (response.data.main.temp);
                 cityElement.innerHTML = response.data.name;
                 descripitionElement.innerHTML = response.data.weather[0].description;
                 humidityElement.innerHTML = response.data.main.humidity;
                 windElement.innerHTML = Math.round (response.data.wind.speed);
+                iconElement.setAttribute(
+                    "src",
+                    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+                );
+                
                 let currentDate = new Date();
                 let date = document.querySelector("#date");
                 let weekdays = [
@@ -38,6 +45,7 @@
                 date.innerHTML = `${weekdays[currentDate.getDay()]} ${months[currentDate.getUTCMonth()]
                     } ${currentDate.getDate()}`;
                 }
+                
                 var dt = new Date();
                 document.getElementById("time").innerHTML = (("0" + dt.getHours()).slice(-2)) + ":" + (("0" + dt.getMinutes()).slice(-2));
                 
@@ -53,5 +61,6 @@
                     document.getElementById("format").innerHTML = greeting;
 
                 let apiKey = "4603cd08f9aa4435f5a1a0fde738051c";
-                let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Markham&appid=${apiKey}&units=metric`;
+                let city = "Markham";
+                let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
                 axios.get(apiUrl).then(displayTemperature);
